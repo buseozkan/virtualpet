d0f14f0f011e08811d10d0d197ae569bcca207b4
buseozkan <buse1.ozkan@gmail.com>
2021-01-22 23:46:28 +0200

    tests

--- tests/pet.test.js
+++ tests/pet.test.js
@@ -3,12 +3,39 @@ const pet = new Pet('Fido');
 pet.name;
 
 describe('constructor', () => {
-    test('sets the name property', () => {
-        const pet = new Pet('Fido');
-
-        expect(pet.name).toEqual('Fido');
+    test('should be an instance of object', () => {
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
-    // test('returns an object', () => {
-      //  expect(new Pet("Fido")).toBeInstanceOf(Object);
-   // });
 });
+
+describe('constructor', () => {
+    it('should return a pet with a name, age, hunger level(0) and fitness(10)', () => {
+        expect(new Pet('Fido')).toEqual({
+            name: 'Fido',
+            age: 0,
+            hunger: 0, 
+            fitness: 10,
+        });
+    });
+});
+
+describe('growUp', () => {
+    it('should check if the age increases or not', () => {
+        let testPet = new Pet('Test Pet');
+        testPet.growUp(1);
+        expect(testPet.age).toBe(1);
+        testPet.age = 0;
+        testPet.growUp(2);
+        expect(testPet.age).toBe(2);
+    });
+});
+
+describe('health impairment with growUp', () => {
+    it('should check if the pets health impairs when it gets older; age should increase by the numebr passed', () => {
+        let testPet = new Pet('Test Pet')
+        testPet.growUp(1)
+        expect(testPet.hunger).toBe(5);
+        expect(testPet.fitness).toBe(7);
+    });
+});
+
