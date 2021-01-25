facb87d0709aa838f447b5a6a235070fd3f89ccd
buseozkan <buse1.ozkan@gmail.com>
2021-01-25 21:17:31 +0200

    test

--- tests/pet.test.js
+++ tests/pet.test.js
@@ -8,7 +8,7 @@ describe('constructor', () => {
     });
 });
 
-describe('constructor', () => {
+describe('create a new Pet', () => {
     it('should return a pet with a name, age, hunger level(0) and fitness(10)', () => {
         expect(new Pet('Fido')).toEqual({
             name: 'Fido',
@@ -16,6 +16,16 @@ describe('constructor', () => {
             hunger: 0, 
             fitness: 10,
         });
+    });
+});
+
+describe('growUp', () => {
+    it('should check if the age increases', () => {
+        Pet.growUp(1);
+        expect(Pet.age).toBe(1);
+        Pet.age = 0;
+        Pet.growUp(2);
+        expect(Pet.age).toBe(2);
     });
 });
 
@@ -38,23 +48,5 @@ describe('feed', () => {
     });
 });
 
-//describe('growUp', () => {
-  //  it('should check if the age increases or not', () => {
-    //    let testPet = new Pet('Test Pet');
-      //  testPet.growUp(1);
-        //expect(testPet.age).toBe(1);
-        //testPet.age = 0;
-        //testPet.growUp(2);
-        //expect(testPet.age).toBe(2);
-    //});
-//});
 
-//describe('health with growUp', () => {
-  //  it('should check if the pets health impairs when it gets older; age should increase by the numebr passed', () => {
-    //    let testPet = new Pet('TestPet')
-      //  testPet.growUp(1)
-        //expect(testPet.hunger).toBe(5);
-        //expect(testPet.fitness).toBe(7);
-    //});
-//});
 
